bd515f961f722fe14e0020ffa9234002052778ad
xiazx <154128293@qq.com>
2020-11-18 10:52:48 +0800

    添加项目文件。

---
+++ myTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
